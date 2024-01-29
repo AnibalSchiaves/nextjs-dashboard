4fe792039923707ef0c84f24f765a8f7e7ce0d51
Aníbal Schiaves <aschiav0@rosario.gov.ar>
2024-01-29 15:01:06 -0300

    delete invoices

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
   return (
@@ -25,12 +28,19 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const confirmDeleteInvoiceById = (formData: FormData) => {
+    const deleteInvoiceById = deleteInvoice.bind(null, id);
+    if (confirm("Are you sure?")) {
+      deleteInvoiceById();
+    }
+  }
+
   return (
-    <>
+    <form action={confirmDeleteInvoiceById}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
